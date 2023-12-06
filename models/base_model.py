63f1f54cd1a17ca2281a604a8e983e297b0f94aa
Juweria86 <jujumelmi143@gmail.com>
2023-12-06 08:37:11 +0300

    updating base class

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Defines the BaseModel class."""
+import models
 from datetime import datetime
 import uuid
-"""Defines the BaseModel class."""
 
 
 class BaseModel:
@@ -16,19 +17,23 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        isof = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
                 if key.endswith('at') and isinstance(value, str):
-                    self.__dict__[key] = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__[key] = datetime.strptime(value, isof)
                 else:
                     self.__dict__[key] = value
+        else:
+            models.storage.new(self)
 
     def save(self):
         """updates update_at attribute with current time"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all 
+        """returns a dictionary containing all
         keys/values of __dict__ of the instance"""
         i_dict = self.__dict__.copy()
         i_dict["created_at"] = self.created_at.isoformat()
