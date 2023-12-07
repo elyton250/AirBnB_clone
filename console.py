804b9c364880032b86f5ae2bb77da503fb07335e
elyton250 <nelysemathieu@gmail.com>
2023-12-07 23:32:15 +0200

    all and update

--- console.py
+++ console.py
@@ -102,6 +102,107 @@ class HBNBCommand(cmd.Cmd):
             print("user destroyed and saved")
             return
 
+    def do_all(self, args):
+        """this prints a string represantation of an instance"""
+        
+        instances = storage.all()
+        
+        if args:
+            for key, value in instances.items():
+                if args == key.split('.')[0]:
+                    print(str(instances[key]))
+        else:
+            print(str(instances))
+
+
+    def do_update(self, arg):
+        
+        if not arg:
+            print("** class name missing **")
+            return
+
+        args = arg.split()
+        
+        cls_name = args[0]
+        idd = args[1]
+        attr_name = args[2]
+        attr_val = args[3]
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        instances = storage.all()
+        
+        cls_name_list = [key.split('.')[0] for key in instances.keys()]
+        
+        if cls_name not in cls_name_list:
+            print("** class doesn't exist **")
+            return
+
+        key_to_up = "{}.{}".format(cls_name, idd)
+        
+        keys_list = list(instances.keys())
+            
+        if key_to_up not in keys_list:
+            print("** no instance found **")
+            return
+        else:
+            if len(args) < 3:
+                print("** attribute name missing **")
+                return
+            elif len(args) < 4:
+                print("** value missing **")
+                return
+            else:
+                attr_value = self.cast_type(attr_val)
+                setattr(instances[key_to_up], attr_name, attr_value)
+                storage.save()
+                return
+    @staticmethod 
+    def cast_type(attr_value):
+        """Convert attr_value to the appropriate type by trying multiple types"""
+
+        possible_types = [int, float, bool, str]
+
+        for data_type in possible_types:
+            try:
+                converted_value = data_type(attr_value)
+                return converted_value
+            except (ValueError, TypeError):
+                pass
+        #there is a bug everything is being casted as bool
+        return attr_value
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
 
 
 
