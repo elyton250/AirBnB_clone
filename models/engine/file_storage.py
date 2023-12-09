4babf6fb1fba85584c820a433d05235ee0868c7c
Juweria86 <jujumelmi143@gmail.com>
2023-12-09 08:44:43 +0300

    fixing some errors

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,23 +12,27 @@ from models.review import Review
 
 class FileStorage:
     """represents FileStorage class"""
+
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """returns the dictionary __objects"""
+
         return (FileStorage.__objects)
 
     def new(self, obj):
         """sets in __objects the obj with key
         obj: object to be set
         """
+
         class_name = obj.__class__.__name__
         FileStorage.__objects["{}.{}".format(class_name, obj.id)] = obj
 
     def save(self):
         """serializes __objects to the
         JSON file (path: __file_path)"""
+
         s_objects = FileStorage.__objects
         s_objects = {obj: s_objects[obj].to_dict() for obj in s_objects.keys()}
         with open(FileStorage.__file_path, 'w') as file:
@@ -36,6 +40,7 @@ class FileStorage:
 
     def reload(self):
         """deserializes the JSON file to __objects"""
+
         try:
             with open(FileStorage.__file_path) as f:
                 objdict = json.load(f)
