94247c455b175cf7d4697ba206e32553be8b53c7
Juweria86 <jujumelmi143@gmail.com>
2023-12-09 08:07:42 +0300

    pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -39,8 +39,8 @@ class BaseModel:
         i_dict["created_at"] = self.created_at.isoformat()
         i_dict["updated_at"] = self.updated_at.isoformat()
         i_dict['__class__'] = self.__class__.__name__
-        return i_dict
+        return (i_dict)
 
     def __str__(self):
         """prints Base_model instances"""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
